a540cbd5752fa7823a8f1b081820ea2b47e354cf
Raudy2 <maxbeltre@gmail.com>
2022-10-19 14:45:04 -0400

    add linux platforms

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-admin = User.create(name: "klaus", email: 'klaus@admin.com', password: '10168913', password_confirmation: '10168913')
-admin.save
-editor = User.create(name: "akashi", email: 'akashi@editable.com', password: '10168913', password_confirmation: '10168913')
-editor.save
-User.create(name: "Max", email: 'max@normal.com', password: '10168913', password_confirmation: '10168913')
+admin = User.create!(name: "klaus", email: 'klaus@admin.com', password: '10168913', password_confirmation: '10168913')
+editor = User.create!(name: "akashi", email: 'akashi@editable.com', password: '10168913', password_confirmation: '10168913')
+User.create!(name: "Max", email: 'max@normal.com', password: '10168913', password_confirmation: '10168913')
 
-admin_user_role = Role.create(name: 'Admin User', reference: "User", access: :createable)
-admin_post_role = Role.create(name: 'Admin Post', reference: "Post", access: :createable)
-editor_post_role = Role.create(name: 'Editor Post', reference: "Post", access: :editable)
+admin_user_role = Role.create!(name: 'Admin User', reference: "User", access: :createable)
+admin_post_role = Role.create!(name: 'Admin Post', reference: "Post", access: :createable)
+editor_post_role = Role.create!(name: 'Editor Post', reference: "Post", access: :editable)
 
-admin.user_roles.create(role: admin_user_role)
-admin.user_roles.create(role: admin_post_role)
-editor.user_roles.create(role: editor_post_role)
+admin.user_roles.create!(role: admin_user_role)
+admin.user_roles.create!(role: admin_post_role)
+editor.user_roles.create!(role: editor_post_role)
 
 # FactoryBot.create_list(:post, 40)
